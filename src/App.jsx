eb777674a3efe30496dfd9c39ca7de6dd3ee065e
tusharchawre <tusharchawre0240@gmail.com>
2024-12-20 19:49:55 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+
+
+  return (
+    <>
+    <div>
+       <Navbar />
+    </div>
+    </>
+  )
+}
+
+export default App
